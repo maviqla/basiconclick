c648c4dad975d834ee7db9ddacf00a3cd1636f61
Victoria Maria <victoria.prjt@gmail.com>
2021-11-26 10:28:01 -0300

    ta bugado

--- onclick.js
+++ onclick.js
@@ -1,6 +1,28 @@
+function addAnswer() {
 
+    submeterReply();
+  
+    return true;
+  }
 
 function submeterReply() {
-    document.getElementById("subMensagem").innerHTML = "<h3>Obrigado por participar!<h3>";
+    let checkvalue = document.getElementById("checkvalue").value;
+    let firstradiovalue = document.getElementById("firstradiovalue").value;
+    let textboxvalue = document.getElementById("textboxvalue").value;
+    let secondradiovalue = document.getElementById("secondradiovalue").value;
+    let tableBody = document.getElementById("table-body");
+
+    let html = "<tr>";
+  html += "<td>" + checkvalue + "</td>";
+  html += "<td>" + firstradiovalue + "</td>";
+  html += "<td>" + textboxvalue + "</td>";
+  html += "<td>" + secondradiovalue + "</td>";
+  html += "</tr>";
+  
+  tableBody.innerHTML += html;
+
+  return true;
+
+    
    
 }
